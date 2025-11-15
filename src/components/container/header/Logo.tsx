c02bc6c80128d92559b2854c1584ae42aaf20385
Cindy Nufa <cindynufa@gmail.com>
2025-11-16 01:48:14 +0700

    Feat: added logo at header section, and import from Home page for first test

---
+++ src/components/container/header/Logo.tsx
@@ -0,0 +1,10 @@
+import Image from '../../ui/image';
+
+export default function Logo() {
+  return (
+    <div>
+      <Image src="/icons/logo.png" alt="Logo" className="w-10.5 h-10.5" />
+      <h1>Booky</h1>
+    </div>
+  );
+}
---
+++ src/components/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import Header from '../../container/header';
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  );
+}
