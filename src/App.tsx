bf28f98293af3d1b43b7b1faeca2b5de37eed75b
Cindy Nufa <cindynufa@gmail.com>
2025-11-16 01:51:01 +0700

    Feat: added App Provider to fetching data and import from App.tsx to show to the browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-
-
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router';
+import AppProvider from './providers/AppProvider';
+import Home from './components/pages/home';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<Home></Home>}></Route>
+      </Route>
+    )
+  );
 
   return (
-    <>
-      
-    </>
-  )
+    <AppProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AppProvider>
+  );
 }
 
-export default App
+export default App;
