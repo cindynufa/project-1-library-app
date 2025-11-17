789b4bfb9e71d084a713ae0751385f3a5081be63
Cindy Nufa <cindynufa@gmail.com>
2025-11-17 13:17:03 +0700

    Feat: added constant for hero image carousel

---
+++ src/constants/HeroImage.ts
@@ -0,0 +1,11 @@
+type HeroImage = {
+  src: string;
+};
+
+const HeroImageConst: HeroImage[] = [
+  { src: '/images/hero.png' },
+  { src: '/images/hero.png' },
+  { src: '/images/hero.png' },
+];
+
+export default HeroImageConst;
