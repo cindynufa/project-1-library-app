e6652bca192e3345a0ba790177cdadc2458319f9
Cindy Nufa <cindynufa@gmail.com>
2025-11-17 10:08:26 +0700

    Feat: added footer section, desktop version

---
+++ src/components/container/footer/index.tsx
@@ -0,0 +1,36 @@
+import SocialMediaConst from '../../../constants/SocialMedia';
+import Image from '../../ui/image';
+import Logo from '../../ui/logo';
+
+export default function Footer() {
+  return (
+    <footer className="fixed flex flex-col w-full px-37.5 py-20 gap-10 bottom-0 border-t border-neutral-300 justify-between items-center">
+      <div className="flex flex-col gap-5.5 items-center">
+        <Logo />
+        <p className="text-md font-semibold text-neutral-950">
+          Discover inspiring stories & timeless knowledge, ready to borrow
+          anytime. Explore online or visit our nearest library branch.
+        </p>
+      </div>
+      <div className="flex flex-col gap-5 items-center">
+        <p className="text-md font-bold text-neutral-950">
+          Follow on Social Media
+        </p>
+        <div className="flex flex-row gap-3">
+          {SocialMediaConst.map((socialMedia, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 border border-neutral-300 rounded-full"
+            >
+              <Image
+                src={socialMedia.src}
+                alt="Social Media"
+                className="mx-auto translate-y-1/2"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/constants/SocialMedia.ts
@@ -0,0 +1,12 @@
+type SocialMedia = {
+  src: string;
+};
+
+const SocialMediaConst: SocialMedia[] = [
+  { src: '/icons/socmed-fb.png' },
+  { src: '/icons/socmed-ig.png' },
+  { src: '/icons/socmed-in.png' },
+  { src: '/icons/socmed-tt.png' },
+];
+
+export default SocialMediaConst;
