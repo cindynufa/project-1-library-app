3cb86947e0488d4448ac97417bd50bc2748579f8
Cindy Nufa <cindynufa@gmail.com>
2025-11-19 00:24:39 +0700

    Feat: added variant button for reusable

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,11 +4,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "../../lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium cursor-pointer transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-md font-bold cursor-pointer transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
-        default: "w-50 h-12 p-2 gap-2 border border-neutral-300 rounded-full bg-white text-neutral-950 hover:bg-primary-100c hover:text-white",
+        default: "w-50 h-12 p-2 gap-2 border border-neutral-300 rounded-full bg-white  text-neutral-950 hover:bg-primary-100c hover:text-white",
         destructive:
           "w-50 h-12 p-2 gap-2 border border-neutral-300 rounded-full bg-primary-100c text-white hover:bg-white hover:text-black focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-white",
         outline:
@@ -18,6 +18,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        borrowbook: "w-50 h-12 p-2 gap-2 border border-neutral-300 rounded-full bg-primary-100c text-white hover:bg-white hover:text-black",
+        share: "w-11 h-11 px-4 py-3 gap-3 border border-neutral-300 rounded-full bg-background hover:bg-neutral-300",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
