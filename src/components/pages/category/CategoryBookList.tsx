bfe017728fb1dfcf7c45156555f936b812aaf9a1
Cindy Nufa <cindynufa@gmail.com>
2025-11-19 15:00:54 +0700

    Feat: added link from book list to detail page

--- src/components/pages/category/CategoryBookList.tsx
+++ src/components/pages/category/CategoryBookList.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import BookCard from '../../ui/bookCard';
 
 export default function CategoryBookList() {
@@ -5,12 +6,14 @@ export default function CategoryBookList() {
     <section className="grid grid-cols-4 w-full gap-5 justify-between">
       {Array.from({ length: 8 }).map((_, index) => (
         <div key={index}>
-          <BookCard
-            src="/images/book-11.png"
-            title="Book Name"
-            author="Author name"
-            rate={4.9}
-          />
+          <Link to="/Detail">
+            <BookCard
+              src="/images/book-11.png"
+              title="Book Name"
+              author="Author name"
+              rate={4.9}
+            />
+          </Link>
         </div>
       ))}
     </section>
