2b17e1ea7ecf6adda0de9b79ee49dfdc51b4194c
Cindy Nufa <cindynufa@gmail.com>
2025-11-19 20:58:51 +0700

    Feat: added more padding for main content

--- src/components/pages/cart/index.tsx
+++ src/components/pages/cart/index.tsx
@@ -4,7 +4,7 @@ import CartSummary from './CartSummary';
 
 export default function Cart() {
   return (
-    <main className='flex flex-col gap-8 justify-between'>
+    <main className='flex flex-col gap-8 px-25 justify-between'>
       <SectionTitle heading="My Cart" />
       <div className='flex flex-row gap-10'>
       <CartList/>
