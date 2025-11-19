cd4dbc05e63848b07ba381d0a8b502b010d54f10
Cindy Nufa <cindynufa@gmail.com>
2025-11-19 18:15:14 +0700

    Feat: added property category, titleClassname, and children

--- src/components/ui/BookTitle.tsx
+++ src/components/ui/BookTitle.tsx
@@ -1,29 +1,30 @@
-import Image from './image';
+import type { ReactNode } from 'react';
 
 interface BookTitleProps {
   category?: string;
   title: string;
   author: string;
-  rate?: number;
+  children?: ReactNode;
+  titleClassName: string;
 }
 
 export default function BookTitle({
   category,
   title,
   author,
-  rate,
+  children,
+  titleClassName,
 }: BookTitleProps) {
   return (
     <div className="flex flex-col gap-1">
       <h4 className="w-fit px-2 gap-2 border border-neutral-300 rounded-sm text-sm font-bold text-neutral-950">
         {category}
       </h4>
-      <h3 className="text-display-sm font-bold text-neutral-950">{title}</h3>
+      <h3 className={`${titleClassName} text-neutral-950`}>
+        {title}
+      </h3>
       <p className="text-md font-semibold text-neutral-700">{author}</p>
-      <div className="flex flex-row gap-0.5">
-        <Image src="/icons/rate-star.png" alt="Rate Star" className="w-6 h-6" />
-        <p className="text-md font-bold text-neutral-900">{rate}</p>
-      </div>
+      <div className="flex flex-row gap-0.5">{children}</div>
     </div>
   );
 }
