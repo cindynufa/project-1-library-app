54f72348d942710378a3d2c32c9369fd5d65fe8f
Cindy Nufa <cindynufa@gmail.com>
2025-11-19 09:09:24 +0700

    Feat: installed input shadcn ui, modified to search box

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -1,4 +1,5 @@
 import { Search } from 'lucide-react';
+import { Input } from './input';
 
 interface SearchProps {
   text: string;
@@ -6,15 +7,13 @@ interface SearchProps {
 
 export default function SearchBox({ text }: SearchProps) {
   return (
-    <div className="relative w-125 h-11 px-4 py-2 border border-neutral-300 gap-1.5 rounded-full items-center">
-      <div className="absolute flex items-center gap-1.5">
-        <Search className="w-3.75 h-3.75 text-neutral-600" />
-        <input
-          type="text"
-          placeholder={text}
-          className="text-sm font-medium text-neutral-600 border border-white"
-        />
-      </div>
+    <div className="flex w-125 h-11 px-4 py-2 border border-neutral-300 gap-1.5 rounded-full items-center">
+      <Search className="w-3.75 h-3.75 text-neutral-600" />
+      <Input
+        type="text"
+        placeholder={text}
+        className="grow p-0 border-0 text-sm font-medium text-neutral-600"
+      />
     </div>
   );
 }
