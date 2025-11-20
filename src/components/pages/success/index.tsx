f2ff68fabd5dbeeeb0c96bafa4e0807508d2fb29
Cindy Nufa <cindynufa@gmail.com>
2025-11-21 01:36:26 +0700

    Feat: first commit Success page, desktop version

---
+++ src/components/pages/success/index.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router';
+import { Button } from '../../ui/button';
+import Image from '../../ui/image';
+import SectionSubtitle from '../../ui/sectionSubtitle';
+
+export default function Success() {
+  return (
+    <section className="flex flex-col w-full mx-auto gap-8 items-center">
+      <Image
+        src="/icons/success.png"
+        alt="Icon Success"
+        className="size-142.38"
+      />
+
+      <div className="flex flex-col gap-2 text-center">
+        <SectionSubtitle
+          heading="Borrowing Successful!"
+          className="text-display-sm"
+        />
+        <p className="text-lg font-semibold text-neutral-950">
+          Your book has been successfully borrowed. Please return it by{' '}
+          <span className="text-accent-red">31 August 2025</span>{' '}
+        </p>
+      </div>
+
+      <Link to="/Borrowed">
+        <Button variant={'borrowbook'} className="w-71.5 h-12 p-2 gap-2">
+          See Borrowed List
+        </Button>
+      </Link>
+    </section>
+  );
+}
