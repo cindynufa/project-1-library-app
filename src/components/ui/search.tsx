5a50864226a734e3fd032bb99a8b6b5f64a80eba
Cindy Nufa <cindynufa@gmail.com>
2025-11-20 21:47:27 +0700

    Fix: fixed border of search box

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -7,13 +7,13 @@ interface SearchProps {
 
 export default function SearchBox({ text }: SearchProps) {
   return (
-    <div className="flex w-125 h-11 px-4 py-2 border border-neutral-300 gap-1.5 rounded-full items-center">
-      <Search className="w-3.75 h-3.75 text-neutral-600" />
+    <div className="relative flex w-125 items-center">
       <Input
         type="text"
         placeholder={text}
-        className="grow p-0 border-0 text-sm font-medium text-neutral-600"
+        className="h-11 px-4 py-2 border border-neutral-300 rounded-full text-sm font-medium text-neutral-600"
       />
+      <Search className="absolute w-3.75 h-3.75 text-neutral-600" />
     </div>
   );
 }
