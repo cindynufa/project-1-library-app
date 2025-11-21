edd5f14dc570581e603076ab24b6744c69e6d5a4
Cindy Nufa <cindynufa@gmail.com>
2025-11-22 02:04:34 +0700

    Feat: User Profile page, desktop version

--- src/components/pages/user/UserProfile.tsx
+++ src/components/pages/user/UserProfile.tsx
@@ -1,13 +1,28 @@
+import { Button } from '../../ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '../../ui/card';
+import Image from '../../ui/image';
+import UserCard from '../../ui/userCard';
 
 export default function UserProfile() {
   return (
-    <Card>
+    <Card className="flex flex-col gap-6">
       <CardHeader>
-        <CardTitle></CardTitle>
+        <CardTitle className="text-display-sm font-bold">Profile</CardTitle>
       </CardHeader>
-      <CardContent>
-        <div></div>
+      <CardContent className="flex flex-col w-139.25 p-5 gap-6 border-0 bg-white rounded-2xl shadow">
+        <Image
+          src="/images/photo-profile.png"
+          alt="Photo Profile"
+          className="size-16"
+        />
+        <UserCard
+          name="Johndoe"
+          email="johndoe@email.com"
+          phone="081234567890"
+        />
+        <Button variant={'borrowbook'} className="w-full h-11 p-2 gap-2">
+          Update Profile
+        </Button>
       </CardContent>
     </Card>
   );
