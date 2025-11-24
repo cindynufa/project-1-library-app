8654d7f5a75cb048e96e19250b456a54f6741a62
Cindy Nufa <cindynufa@gmail.com>
2025-11-24 17:09:55 +0700

    Feat: modified logo, adjust with logo ui component

--- src/components/container/header/index.tsx
+++ src/components/container/header/index.tsx
@@ -6,7 +6,7 @@ import HeaderProfile from './HeaderProfile';
 export default function Header() {
   return (
     <header className="fixed flex w-full top-0 z-50 h-20 px-30 justify-between items-center bg-white shadow">
-      <Logo />
+      <Logo classnameImage="size-10.5" classnameBrand="text-display-md" />
       <SearchBox text="Search book" />
       <div className="flex flex-row gap-6 justify-between items-center">
         <HeaderCart />
