8b9c1e6af2964553ba553c44477c1015bdc96c4c
Cindy Nufa <cindynufa@gmail.com>
2025-11-24 17:09:15 +0700

    Feat: modified classname as a type for image and brand

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router';
 import Image from './image';
 
-export default function Logo() {
+interface LogoProps {
+  classnameImage?: string;
+  classnameBrand?: string;
+}
+
+export default function Logo({classnameImage, classnameBrand}: LogoProps) {
   return (
     <Link to="/">
       <div className="flex flex-row gap-3.75 items-center">
-        <Image src="/icons/logo.png" alt="Logo" className="w-10.5 h-10.5" />
-        <h1 className="text-display-md font-bold text-neutral-950">Booky</h1>
+        <Image src="/icons/logo.png" alt="Logo" className={`${classnameImage} `} />
+        <h1 className={`${classnameBrand} font-bold text-neutral-950`}>Booky</h1>
       </div>
     </Link>
   );
